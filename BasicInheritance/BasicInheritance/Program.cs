e4c24ef08aef3b7e3a1d59c5727548fd1d17878f
СайынБелек Алексеевич Кошкароол <sakoshkarool@ttit.local>
2018-02-07 09:21:59 +0700

    Add

---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+
+            MiniVan myVan = new MiniVan();
+
+            myVan.Speed = 10;
+            Console.WriteLine("My van is going {0} MPH", myVan.Speed);
+            Console.ReadLine();
+        }
+    }
+}
